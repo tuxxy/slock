9332b0c1511c0bdd140869f86ae8c874b16aa430
tuxxy <me@johnpacific.com>
2017-09-04 00:13:26 -0600

    Rename macro LOCK_ATTEMPTS_SIREN to LOCK_ATTEMPTS_ALARM

--- config_example.h
+++ config_example.h
@@ -1,8 +1,8 @@
 // Max number of unlock attempts before shutdown
 #define LOCK_ATTEMPTS_MAX 5
 
-// Max number of unlock attempts before siren alarm
-#define LOCK_ATTEMPTS_SIREN 2
+// Max number of unlock attempts before alarm
+#define LOCK_ATTEMPTS_ALARM 2
 
 // Slock directory within $HOME
 #define SLOCK_DIRECTORY "/slock"
